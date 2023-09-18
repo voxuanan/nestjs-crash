bbb18b9066a3bc1dc9378f9c9e7977f169a05053
Voxuanan <voxuanan0307@gmail.com>
2023-09-18 09:49:51 +0700

    socket-io-redis-adapter

---
+++ socket-io-emit/README.md
@@ -0,0 +1,4 @@
+Code ví dụ cho trường hợp sử dụng socket-io dùng trong môi trường production (BE được deploy mutiple instance, và client trỏ tới BE qua load balance sử dụng round robin) -> sử dụng redis để broadcast giữa các instance
+
+<img src="image1.png" alt="preview-img" />
+<img src="image2.png" alt="redis-adapter" />
---
+++ socket-io-emit/nest-socketio-and-redis-adapter/src/events/events.gateway.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Injectable } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+  transports: ['websocket'],
+})
+@Injectable()
+export class EventsGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
+  sendData(message: string) {
+    this.server.emit('EmitData', message);
+  }
+
+  handleConnection(client: any, ...args: any[]): any {
+    console.log(
+      `client ${client.handshake.headers.origin} is connecting on websocket`,
+    );
+  }
+}
